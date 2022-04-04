a7f5e1134b1b27d8f1e71b711542256de809e3f4
qilinZhu <412921397@qq.com>
2022-04-04 20:12:32 +0800

    vue3cms
    vue3cms

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
+  plugins: [
+    [
+      "import",
+      {
+        libraryName: "element-plus",
+        customStyleName: (name) => {
+          return `element-plus/theme-chalk/${name}.css`;
+        }
+      }
+    ]
+  ],
+  presets: ["@vue/cli-plugin-babel/preset"]
 };
